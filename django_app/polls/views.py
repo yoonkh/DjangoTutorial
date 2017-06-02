aac7bb516ca0cc7b426f32d98ad937f3fd01e8f7
yoonkh <zizou0812@gmail.com>
2017-06-02 15:41:20 +0900

    add part3333

--- django_app/polls/views.py
+++ django_app/polls/views.py
@@ -36,8 +36,11 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question,
+    }
+    return render(request, 'polls/results.html', context)
 
 
 def vote(request, question_id):
@@ -59,4 +62,4 @@ def vote(request, question_id):
 
 
     else:
-        return HttpResponse("You're voting on questoin %s." % question_id)
+        return HttpResponse("You're voting on question %s." % question_id)
